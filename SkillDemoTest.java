90d83f293d27d17a757e84f9da68229a73ea77aa
rafegers0n <rafegerson@hotmail.com>
2022-02-04 13:04:05 -0800

    Changes

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void subtract() {
+        assertEquals(1, SkillDemo.subtract(1, 1));
+    }
+}
